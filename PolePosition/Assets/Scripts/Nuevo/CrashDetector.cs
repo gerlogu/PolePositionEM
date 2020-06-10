727210852011493e3a9ef5ed655db1364fe6fc1d
Thund3rDev <samuelriosdev@gmail.com>
2020-06-10 18:28:33 +0200

    Detección completa de colisiones

--- PolePosition/Assets/Scripts/Nuevo/CrashDetector.cs
+++ PolePosition/Assets/Scripts/Nuevo/CrashDetector.cs
@@ -1,27 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using UnityEngine;
+﻿using UnityEngine;
 
+//Clase CrashDetector, que comprueba si el coche ha tenido una colisión.
 public class CrashDetector : MonoBehaviour
 {
-    private GameObject esfera;
-    private WheelCollider[] ruedas;
-    private int contadorSegundos = 0;
-    private float proximoSegundo = 0;
-    public LayerMask whatIsRoad;
-    public float checkRadius = 0.5f;
+    #region Variables privadas
+    private GameObject esfera; //Esfera que se mueve junto al coche
+    private WheelCollider[] ruedas; //Array de ruedas del coche
+    private int contadorSegundos = 0; //Contador de segundos para cuando el coche ha tenido una colisión
+    private float proximoSegundo = 0; //Tiempo de ejecución del próximo segundo para el contador
+    private Vector3 posicionAnterior; //Vector que guarda la posición del vehículo para compararla posteriormente
+    private Transform transformCamera; //Transform de la cámara
+    #endregion
 
+    #region Variables publicas
+    [Tooltip("Layer \"Road\"")] public LayerMask whatIsRoad; //Capa que indica qué es carretera
+    [Tooltip("Radio de detección de la carretera")] public float checkRadius = 0.5f; //Radio de detección de la carretera
+    #endregion
+
+    //Función Start, que inicializa las siguientes variables.
     private void Start()
     {
+        //Se obtiene la esfera del jugador del array que se encuentra en el PolePositionManager
         int playerID = GetComponent<PlayerInfo>().ID;
         esfera = GameObject.Find("@PolePositionManager").GetComponent<PolePositionManager>().m_DebuggingSpheres[playerID];
+
+        //Se obtiene la cámara que sigue al jugador
+        transformCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+        //Se guardan las ruedas en el array
         ruedas = GetComponentsInChildren<WheelCollider>();
+
+        //Se establece la primera posición del coche
+        posicionAnterior = transform.position;
     }
 
-    // Update is called once per frame
+    //Función Update, que se ejecuta cada frame.
     void Update()
     {
+        //Variable booleana para saber si el coche está tocando el suelo
         bool isGrounded = true;
 
         //Para cada rueda se comprueba si colisiona con la carretera
@@ -34,7 +50,7 @@ public class CrashDetector : MonoBehaviour
             }
         }
 
-        //Si una de las ruedas no está tocando el suelo, aumentamos el contador
+        //Si una de las ruedas no está tocando el suelo, se aumenta el contador
         if (!isGrounded)
         {
             if (Time.time >= proximoSegundo)
@@ -45,15 +61,31 @@ public class CrashDetector : MonoBehaviour
         }
         else
         {
-            contadorSegundos = 0;
+            //Si el coche está en el suelo, pero no puede moverse, también se considera
+            float InputAcceleration = Input.GetAxis("Vertical");
+            if (InputAcceleration != 0 && transform.position == posicionAnterior)
+            {
+                if (Time.time >= proximoSegundo)
+                {
+                    contadorSegundos++;
+                    proximoSegundo = Time.time + 1.0f;
+                }
+            }
+            else
+            {
+                contadorSegundos = 0;
+            }
         }
 
         //Si el contador llega a 3, se reinicia la posición del coche
         if (contadorSegundos == 3)
         {
             contadorSegundos = 0;
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transformCamera.rotation.eulerAngles.y, 0);
             transform.position = new Vector3(esfera.transform.position.x, 3.0f, esfera.transform.position.z);
         }
+
+        //Se guarda la posición anterior del coche.
+        posicionAnterior = transform.position;
     }
 }
